d1725b96ada24dde10d27ac3a4aa118abee9eb6f
Anurima2206 <anutri2k05@gmail.com>
2026-02-21 00:08:47 +0530

    Done

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,10 @@ app.use(express.json());
 
 //import routes
 import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
 
 //declares routes
 app.use("/api/v1/users",userRouter);
-
+app.use("/api/v1/posts",postRouter);
 
 export default app;
